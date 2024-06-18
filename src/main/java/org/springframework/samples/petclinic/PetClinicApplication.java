d246ca11cb5fa1084fe905f1c93ff9e7e8379385
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-06-18 23:29:47 +0300

    bytebuddy test

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -16,12 +16,16 @@
 
 package org.springframework.samples.petclinic;
 
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.agent.Installer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.lang.instrument.Instrumentation;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Locale;
 
 /**
@@ -35,7 +39,43 @@ import java.util.Locale;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
+
+		testBytebuddyInstaller();
+
 		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+	private static void testBytebuddyInstaller() {
+
+
+		System.out.println("ByteBuddyAgent Installer class loader is "+ Installer.class.getClassLoader());
+
+		Instrumentation instrumentation = null;
+		try {
+			System.out.println("trying to get instrumentation from bytebuddy Installer");
+			Class<?> installer = Class.forName("net.bytebuddy.agent.Installer", true, ClassLoader.getSystemClassLoader());
+			instrumentation = (Instrumentation) installer.getMethod("getInstrumentation").invoke(null);
+
+			if (instrumentation != null){
+				System.out.println("got instrumentation from Installer");
+			}else{
+				System.out.println("bytebuddy installer in in system class loader but instrumentation is null");
+			}
+
+		} catch (Exception e) {
+			String msg = e.toString();
+			if (e instanceof InvocationTargetException && ((InvocationTargetException) e).getTargetException() != null){
+				msg = ((InvocationTargetException) e).getTargetException().toString();
+			}
+			System.out.println("error trying to get instrumentation from bytebuddy Installer "+msg);
+		}
+
+		if (instrumentation == null){
+			System.out.println("installing byte buddy agent");
+			instrumentation = ByteBuddyAgent.install();
+			System.out.println("got instrumentation after bytebuddy agent install "+instrumentation);
+		}
+
 	}
 
 }
